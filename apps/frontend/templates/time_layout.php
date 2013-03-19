e5f23e0386de8053ae638db8e472253088e8f81d
Myshev <sergey.myshev@softline.ru>
2013-03-20 00:59:06 +0400

    картинки для текста + изменение time_layout.php

--- apps/frontend/templates/time_layout.php
+++ apps/frontend/templates/time_layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<title>Разработка сайтов - от визиток до корпоративных порталов</title>
 	<?//php include_http_metas() ?>
 	<?//php include_metas() ?>
 	<?//php include_title() ?>
