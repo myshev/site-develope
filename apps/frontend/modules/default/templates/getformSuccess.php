225705d9ec2d37df7908f4d02a21c079167fe4e5
Myshev <sergey.myshev@softline.ru>
2013-03-25 16:15:19 +0400

    favicon

--- apps/frontend/modules/default/templates/getformSuccess.php
+++ apps/frontend/modules/default/templates/getformSuccess.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="ru" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
-<head>
-	<title>Мобильные телефоны с доставкой по всей России напрямую от производителя</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link href="/css/common.css" rel="stylesheet">
-	<link href="/css/main.css" rel="stylesheet">
-	<script src="/js/jquery.min.js"></script>
-	<script src="/js/masked_input.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function () {
-			$(".phone-input").mask("+7 (999) 999-99-99");
-
-			$('.b-btn').click(function () {
-				$(this).parent('form').submit();
-			});
-		});
-	</script>
-</head>
-<body>
-	<?if ($sf_user->hasFlash('notice')) {?>
-		<div id="close_button">
-			<p class="ok-message"><?=$sf_user->getFlash('notice') ?></p>
-			<a class="b-btn" href="javascript: return void(0);" onClick="parent.$.fn.colorbox.close();">
-				<span class="b-btn__title">Закрыть окно</span>
-			</a>
-		</div>
-	<?} else {?>
-		<div class="form-order" style="margin: 0 auto; width: 440px; height: 300px;">
-			<?include_partial('default/client_form', array('clientForm'	=> $clientForm, 'router'	=> 'getform'));?>
-		</div>
-	<?}?>
-
-</body>
-</html>
+<?if ($sf_user->hasFlash('notice')) {?>
+	<div id="close_button">
+		<p class="ok-message"><?=$sf_user->getFlash('notice') ?></p>
+		<a class="b-btn" href="javascript: return void(0);" onClick="parent.$.fn.colorbox.close();">
+			<span class="b-btn__title">Закрыть окно</span>
+		</a>
+	</div>
+<?} else {?>
+	<div class="form-order" style="margin: 0 auto; width: 440px; height: 300px;">
+		<?include_partial('default/client_form', array('clientForm'	=> $clientForm, 'router'	=> 'getform'));?>
+	</div>
+<?}?>
--- apps/frontend/templates/time_form_layout.php
+++ apps/frontend/templates/time_form_layout.php
@@ -11,35 +11,16 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link href="/css/old/common.css" rel="stylesheet">
 	<link href="/css/old/main.css" rel="stylesheet">
-	<link href="/css/old/colorbox.css" rel="stylesheet">
-	<link href="/css/old/flexslider.css" rel="stylesheet">
-	<link href="/css/old/screen.css" rel="stylesheet" type="text/css" media="screen" />
 	<script src="/js/old/jquery.min.js"></script>
-	<script src="/js/old/jquery.colorbox.js"></script>
 	<script src="/js/old/masked_input.js"></script>
-	<script src="/js/old/easySlider1.7.js"></script>
 	<script src="/js/old/jquery.flexslider.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
-			$("#slider").easySlider({
-				auto: false,
-				continuous: true
-			});
-
-			$('.flexslider').flexslider({
-				animation: "slide",
-				slideshow: false,
-				animationSpeed: 1000
-			});
-
 			$(".phone-input").mask("+7 (999) 999-99-99");
 
 			$('.b-btn').click(function () {
 				$(this).parent('form').submit();
 			});
-
-			$("a[rel='cb_link']").colorbox({width:"600", height:"400", iframe:true, transition:"fade"});
-			$("a[rel='cb_link_footer']").colorbox({width:"600", height:"400", iframe:true, transition:"fade"});
 		});
 	</script>
 </head>
